c0c3358701293a867fcf0d1d2a6512da0eaf7c2e
hlgilber <hlgilber@calpoly.edu>
2014-03-09 03:54:51 -0700

    Initial configurations

---
+++ GraceChurchKelseyvilleAwana/GraceChurchKelseyvilleAwana/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GraceChurchKelseyvilleAwana.MvcApplication" Language="C#" %>
---
+++ GraceChurchKelseyvilleAwana/GraceChurchKelseyvilleAwana/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(GraceChurchKelseyvilleAwana.Startup))]
+
+namespace GraceChurchKelseyvilleAwana
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
