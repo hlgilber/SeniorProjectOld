7299951deee75d88c34df266af1e47e31065cc5b
hlgilber <hlgilber@calpoly.edu>
2014-04-20 20:32:32 -0700

    Switching computers

---
+++ GraceChurchKelseyvilleAwana/GraceChurchKelseyvilleAwana/Controllers/EmailController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GraceChurchKelseyvilleAwana.Models;
+
+namespace GraceChurchKelseyvilleAwana.Controllers
+{
+    public class EmailController : Controller
+    {
+        //
+        // GET: /Email/
+        public ActionResult Index(bool confirm = false)
+        {
+            return View(new EmailViewModel { Confirm = confirm });
+        }
+
+        [HttpPost]
+        public ActionResult Index(EmailViewModel vm)
+        {
+            if (vm.Recipients.Equals("Leaders"))
+            {
+                EmailLeaders(vm);
+            }
+            else if (vm.Recipients.Equals("Students"))
+            {
+                EmailStudents(vm);
+            }
+
+            return RedirectToAction("Index", new { @confirm = true });
+        }
+
+        private void EmailStudents(EmailViewModel vm)
+        {
+            
+        }
+
+        private void EmailLeaders(EmailViewModel vm)
+        {
+            
+        }
+	}
+}
---
+++ GraceChurchKelseyvilleAwana/GraceChurchKelseyvilleAwana/Models/EmailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GraceChurchKelseyvilleAwana.Models
+{
+    public class EmailViewModel
+    {
+        [Display (Name = "Message")]
+        public string EmailBody { get; set; }
+
+        [Display (Name = "Recipients")]
+        public string Recipients { get; set; }
+
+        public bool Confirm { get; set; }
+    }
+}
